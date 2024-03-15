cbcdcc7e568f6e33a496df12993f2a11a17983ee
kghstgrl <160274071+kghstgrl@users.noreply.github.com>
2024-03-15 19:59:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,32 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
   int c = 0;
-  int left = 0;
-  int right = size - 1;
-  while (left <= right) {
-      int middle = left + (right - left) / 2;
-      if (arr[middle] == value) {
-          c++;
-          int sleft = middle - 1;
-          while (sleft >= 0 && arr[sleft] == value) {
-              c++;
-              sleft--;
-          }
-          int sright = middle + 1;
-          while (sright < size && arr[sright] == value) {
-              c++;
-              sright++;
-          }
-          return c;
-      }
-      else if (arr[middle] < value) {
-          left = middle + 1;
-      }
-      else {
-      right = middle - 1;
-      }
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == value)
+      c++;
   }
+  return c;
   return 0; // если ничего не найдено
 }
